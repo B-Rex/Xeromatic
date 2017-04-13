ec9923bfede5bdca99d7aef8c9c48ecd058ca135
Will Moreland <will@moreland.nz>
2017-04-14 00:06:49 +1200

    Enlarge stored tweet size in database
    Twitter has introduced a new rule (2016) that allows tweets greater than
    140 characters. This is because usernames and attachment links are no
    longer accounted for in the tweet length.

--- Project/Xeromatic.DB/Tables/Tweet.sql
+++ Project/Xeromatic.DB/Tables/Tweet.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[Tweet]
 (
     [Id]    BIGINT          NOT NULL,
-    [Text]  NVARCHAR(140)   NOT NULL
+    [Text]  NVARCHAR(1000)   NOT NULL
+    /*
+     *  Tweet text size is 1000 because we can no longer assume that it will be 140 characters.
+     *  https://github.com/linvi/tweetinvi/wiki/Extended-Tweets-&-TweetMode
+     */
 
     CONSTRAINT [PK_Subscription] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
